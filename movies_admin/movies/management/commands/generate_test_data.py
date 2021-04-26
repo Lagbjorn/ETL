099ee6450a355deb3223fbd05abce499a73f5201
Likbjorn <dbegal@mail.ru>
2021-04-26 22:07:26 +0300

    Add tqdm for better output

--- movies_admin/movies/management/commands/generate_test_data.py
+++ movies_admin/movies/management/commands/generate_test_data.py
@@ -1,4 +1,5 @@
 import random
+from tqdm import tqdm
 
 from django.db import transaction
 from django.core.management.base import BaseCommand
@@ -19,7 +20,7 @@ from movies.factories import (
 )
 
 NUM_PERSONS = 5_000
-NUM_FILMS = 1_000_000
+NUM_FILMS = 1_000
 NUM_GENRES = 10
 GENRES_PER_FILM = 5
 PERSONS_PER_FILM = 20
@@ -36,16 +37,14 @@ class Command(BaseCommand):
             self.stdout.write(f'Removing {model.__name__} objects...')
             model.objects.all().delete()
 
-        self.stdout.write('Creating new fake data...')
-
         # use generator because NUM_FILMS is LARGE and I want my RAM to stay alive
         films = (FilmWorkFactory() for _ in range(NUM_FILMS))
-        # use list comprehensions because these are smaller & I want to sample from the entire set of these
-        persons = [PersonFactory() for _ in range(NUM_PERSONS)]
-        genres = [GenreFactory() for _ in range(NUM_GENRES)]
+        # use list comprehensions because these are smaller
+        # & I want to sample from the entire set of these
+        persons = [PersonFactory() for _ in tqdm(range(NUM_PERSONS), desc='Creating persons')]
+        genres = [GenreFactory() for _ in tqdm(range(NUM_GENRES), desc='Creating genres')]
 
-        self.stdout.write('Creating fake data relations...')
-        for film in films:
+        for film in tqdm(films, desc="Creating fake FilmWorks", total=NUM_FILMS):
             # sample first to avoid duplicates
             persons_sample = random.sample(persons, random.randint(0, PERSONS_PER_FILM))
             genres_sample = random.sample(genres, random.randint(0, GENRES_PER_FILM))
