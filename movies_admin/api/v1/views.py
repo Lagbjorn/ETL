e32d877eec21f1f8a36213474ceed6a5a52e3fdd
Lagbjorn <dbegal@mail.ru>
2021-05-06 14:48:58 +0300

    Implement simple (not api-friendly) movies ListView

--- movies_admin/api/v1/views.py
+++ movies_admin/api/v1/views.py
@@ -1,10 +1,51 @@
-from django.http import JsonResponse
-from django.views import View
+from django.db.models import QuerySet
+from django.http import Http404, JsonResponse
+from django.views.generic.list import BaseListView
+from django.utils.translation import gettext as _
+
+from movies.models import FilmWork
 
 
-class MoviesListApi(View):
-    http_method_names = ['get']
+class MoviesListApi(BaseListView):
+    paginate_by = 50
+    model = FilmWork
+    http_method_names = ['get']  # Список методов, которые реализует обработчик
 
-    def get(self, request, *args, **kwargs):
-        # Получение и обработка данных
-        return JsonResponse({})
+    def get_queryset(self) -> QuerySet:
+        qs = FilmWork.objects.all().values()
+        return qs
+
+    def get_context_data(self, *, object_list=None, **kwargs) -> dict:
+        qs = self.get_queryset()
+        context = self.paginate_queryset(qs, self.paginate_by)
+        return context
+
+    def paginate_queryset(self, queryset, page_size) -> dict:
+        paginator = self.get_paginator(queryset, page_size)
+
+        page = self.request.GET.get('page', 1)
+        try:
+            page_num = int(page)
+        except ValueError:
+            if page == 'last':
+                page_num = paginator.num_pages
+            else:
+                raise Http404(_('Page is not “last”, nor can it be converted to an int.'))
+
+        page = paginator.get_page(page_num)
+
+        # JSON serializer will convert None to null
+        prev_page_num = page.previous_page_number() if page.has_previous() else None
+        next_page_num = page.next_page_number() if page.has_next() else None
+        result_list = [obj for obj in page.object_list]
+        context = {
+            'count': paginator.count,
+            'total_pages': paginator.num_pages,
+            'prev': prev_page_num,
+            'next': next_page_num,
+            'result': result_list,
+        }
+        return context
+
+    def render_to_response(self, context, **response_kwargs) -> JsonResponse:
+        return JsonResponse(context)
