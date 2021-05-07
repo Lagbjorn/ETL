43fc8fd4ee8b7027cfc7bf9891f1e7feeaedf519
Lagbjorn <dbegal@mail.ru>
2021-05-07 15:29:55 +0300

    Implement correct API for api/v1/movies/ path

--- movies_admin/api/v1/views.py
+++ movies_admin/api/v1/views.py
@@ -1,24 +1,41 @@
-from django.db.models import QuerySet
+from django.contrib.postgres.aggregates import ArrayAgg
+from django.db.models import F, Q, QuerySet
 from django.http import Http404, JsonResponse
 from django.views.generic.list import BaseListView
 from django.utils.translation import gettext as _
 
-from movies.models import FilmWork
+from movies.models import FilmWork, PersonJob
 
 
 class MoviesListApi(BaseListView):
     paginate_by = 50
     model = FilmWork
-    http_method_names = ['get']  # Список методов, которые реализует обработчик
+    http_method_names = ['get']
 
     def get_queryset(self) -> QuerySet:
-        qs = FilmWork.objects.all().values()
+        # look how we just do it in 2 SQL queries!
+        # get queryset for all FilmWorks
+        qs = FilmWork.objects.all().order_by('title').values('id', 'title', 'description', 'creation_date')
+        # get queryset for fields with same names as API specification requires
+        qs = qs.values('id', 'title', 'description', 'creation_date')
+        # add renamed fields; this does not require additional queries to DB
+        qs = qs.annotate(rating=F('imdb_rating'))
+        qs = qs.annotate(type=F('film_type'))
+        # add genres
+        qs = qs.annotate(genres_list=ArrayAgg('genres__genre', distinct=True))
+        # add persons
+        # could have copy 3 times, but we may extend `job` number later
+        for job in PersonJob.values:
+            jobs_list = job + 's'  # like actors_list, writers_list and so on
+            kwarg = {jobs_list: ArrayAgg('persons__name', distinct=True, filter=Q(filmworkperson__job=job))}
+            qs = qs.annotate(**kwarg)
         return qs
 
     def get_context_data(self, *, object_list=None, **kwargs) -> dict:
         qs = self.get_queryset()
         context = self.paginate_queryset(qs, self.paginate_by)
-        context['result'] = [result for result in context['result']]
+        context = dict(context)
+        context['result'] = list(context['result'])
         return context
 
     def paginate_queryset(self, queryset, page_size) -> dict:
@@ -38,6 +55,7 @@ class MoviesListApi(BaseListView):
         # JSON serializer will convert None to null
         prev_page_num = page.previous_page_number() if page.has_previous() else None
         next_page_num = page.next_page_number() if page.has_next() else None
+
         context = {
             'count': paginator.count,
             'total_pages': paginator.num_pages,
